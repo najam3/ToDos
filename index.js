9df9973a73035bd68b9ab4687aa498bf146ccec0
najam3 <100590374+najam3@users.noreply.github.com>
2023-01-19 22:56:02 +0500

    Some features updated
    Additional changes

--- index.js
+++ index.js
@@ -2,29 +2,47 @@ const temp = document.querySelector("#temp").content;
 const input = document.querySelector("#add");
 const btn = document.querySelector("#btn");
 const date = document.querySelector("#date");
-const notes = document.querySelector(".notes")
+const cb = document.querySelector("#accept");
+const li = temp.querySelector("li");
 // Use Date Function
 
+let taskArr = [];
 
 
 const addNotes = () => {
  let x = input.value;
- let li = temp.querySelector("li");
-
- li.innerText = x;
- document.body.appendChild(document.importNode(temp, true))
+ if(x === '') {
+  document.body.appendChild(document.importNode(temp, false))
+ } else {
+   let li = temp.querySelector("li")
+   li.innerText = x;
+   document.body.appendChild(document.importNode(temp, true))
+ }
 }
-const weekday = ["Sun","Mon","Tue","Wed","Thurs","Fri","Satur"];
+const weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 
 const getDate = () => {
-  let input = date.value;
-  const dateAdded = new Date(input);
+   let input = date.value;
+   const dateAdded = new Date(input);
    let span = temp.querySelector("span");
    span.innerText = `${weekday[dateAdded.getDay()]}`;
 }
+
+
 
 
 btn.onclick = () => {
   date.addEventListener("change", getDate())
   addNotes();
 }
+
+//Modal 
+const details = document.getElementById("task-details");
+const btnDetail = document.getElementById("btn-detail");
+
+
+btnDetail.addEventListener("click", () => {
+    getTime()
+});
+
+
