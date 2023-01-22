cee7581e275abfb9bed4424795c3bb0ca8ab3802
najam3 <100590374+najam3@users.noreply.github.com>
2023-01-22 09:26:38 +0500

    Re-do of the App
    Added Functionality, Errors handled.

--- index.js
+++ index.js
@@ -1,48 +1,116 @@
-const temp = document.querySelector("#temp").content;
-const input = document.querySelector("#add");
-const btn = document.querySelector("#btn");
-const date = document.querySelector("#date");
-const cb = document.querySelector("#accept");
-const li = temp.querySelector("li");
-// Use Date Function
+const notesInput = document.querySelector("#notes-input");
+const dateInput = document.querySelector("#date");
+const addBtn = document.querySelector("#add-btn");
+const updateNode = document.querySelector(".added-notess");
+const time = document.querySelector(".time");
+const modal = document.querySelector(".modal")
+const edit = document.querySelector("#edit-btn");
 
-let taskArr = [];
+const createNode = note => {
+  let noteContent = document.createElement("li");
+  noteContent.setAttribute("id", "content")
+  let detailsBtn = document.createElement("span");
+  detailsBtn.style.cursor = 'pointer'
+  detailsBtn.setAttribute("id", "boo")
+  let div = document.createElement("div");
+  detailsBtn.style.width = "max-content"
+  div.setAttribute("class", 'flex-container'); //Add flexbox property
+  let buttonsContainer = document.createElement("div");
+  let remove = document.createElement("span");
+  remove.innerHTML = "&times;"
+  remove.style.fontSize = "28px";
+  remove.style.color = "rgb(128, 255, 133)";
+remove.setAttribute("id", "removeNote")
+  buttonsContainer.append(detailsBtn, remove);
+  buttonsContainer.style.display = "flex";
+  buttonsContainer.style.alignItems = "center";
+  buttonsContainer.style.gap = "0.5em";
+  buttonsContainer.style.color = "green";
+  buttonsContainer.style.flexDirection = "row";
+  buttonsContainer.style.justifyContent = "flex-end";
+ 
+  noteContent.innerText = note;
+  detailsBtn.innerHTML = '&#8711;'
+  detailsBtn.style.marginLeft = '4.5em';
+  noteContent.setAttribute("contenteditable", false)
+  detailsBtn.style.fontSize = '24px'
+  detailsBtn.style.color= '#80FF85'
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.name = "name";
+  checkbox.value = "value";
+  checkbox.id = "check-id";
+  
+   div.style.margin = '0 1em'
+   div.style.padding = '2em 2em'
+  
+  noteContent.style.padding = "0em 0";
+  noteContent.style.fontSize = "16px"
 
+  noteContent.style.alignItems = "center";
+  div.append(checkbox, noteContent, buttonsContainer);
+   updateNode.append(div); 
+};
 
-const addNotes = () => {
- let x = input.value;
- if(x === '') {
-  document.body.appendChild(document.importNode(temp, false))
- } else {
-   let li = temp.querySelector("li")
-   li.innerText = x;
-   document.body.appendChild(document.importNode(temp, true))
- }
-}
-const weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-
-const getDate = () => {
-   let input = date.value;
-   const dateAdded = new Date(input);
-   let span = temp.querySelector("span");
-   span.innerText = `${weekday[dateAdded.getDay()]}`;
-}
-
-
-
-
-btn.onclick = () => {
-  date.addEventListener("change", getDate())
-  addNotes();
-}
-
-//Modal 
-const details = document.getElementById("task-details");
-const btnDetail = document.getElementById("btn-detail");
-
-
-btnDetail.addEventListener("click", () => {
-    getTime()
+updateNode.addEventListener("click", e => {
+     const date = dateInput.value;
+     const note = e.target.parentElement.parentElement.children[1].innerText
+     const quote = document.querySelector("#noted");
+     console.log(note);
+     const newDate = document.querySelector("#dated")
+     e.target.id === 'boo' ? modal.style.display = 'block' : modal.style.display = 'none' ;
+     e.target.id === 'removeNote' ?  e.target.parentElement.parentElement.remove() : 'not'
+     document.body.style.backgroundColor = "rgba(0, 0, 0, 70%)"  
+     quote.innerText =  note;
+   
+  date === "" ? newDate.innerText = "Date Not Added" :   newDate.innerText = new Date(date).toLocaleDateString();
+   
 });
 
+
+
+
+
+
+
+addBtn.onclick = () => {
+ let data = notesInput.value;
+ const newTime = document.querySelector("#time");
+ const check = document.querySelector("#checkId");
+ newTime.innerText = "-Added " + new Date().toLocaleTimeString();
+
+ data === "" ? alert("Add Notes First") :  createNode(data);
+  notesInput.value = "";
+ 
+  // check.addEventListener("onchange", e => {
+  //   check.checked ? e.target.parent.style.backgroundColor = "green" :  e.target.parent.style.backgroundColor = "black"
+  // })
+
+
+
+};
+
+
+edit.addEventListener("click", (e) => {
+  const content = document.querySelector("#content");
+  content.setAttribute("contenteditable", "true");
+  content.style.border = "1px solid red"
+  content.setAttribute("title", "Press Enter");
+ 
+  content.addEventListener("keydown", (e) => {
+   let enter = 'Enter';
+   if(e.key === enter) {
+    e.preventDefault()
+    content.setAttribute("contenteditable", "false");
+    content.style.border = "0"
+   
+   }
+  })
+
+})
+
+
+document.querySelector("#close-modal").addEventListener("click", () => {
+    modal.style.display = "none";
+});
 
